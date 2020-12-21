039599bcdd22f917de3b6f2641e7251c2c78e1de
sfctl <catalbasifa@gmail.com>
2020-12-21 14:55:14 +0300

    Initial Commit

---
+++ TimerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by Yasemin YEL on 21.12.2020.
+//  Copyright © 2020 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
